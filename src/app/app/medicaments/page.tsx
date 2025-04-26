1c90a50f9cf5a42bda8c313d3ab9df14fd68d3f7
vmyronyuk <vitalikmur38@gmail.com>
2025-04-26 10:57:53 +0300

    Fix

--- src/app/app/medicaments/page.tsx
+++ src/app/app/medicaments/page.tsx
@@ -1,6 +1,8 @@
 import { MedicamentsPageContent } from '@/features/medicaments/components/MedicamentsPageContent'
 import { getAllMedicaments } from '@/features/medicaments/dal'
 
+export const dynamic = 'force-dynamic'
+
 export default async function MedicamentsPage() {
 	const medicaments = await getAllMedicaments()
 
