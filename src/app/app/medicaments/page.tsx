41d0d4b56df4022c18ee98086e712f4c3250fbfe
vmyronyuk <vitalikmur38@gmail.com>
2025-04-26 10:52:35 +0300

    Fix

--- src/app/app/medicaments/page.tsx
+++ src/app/app/medicaments/page.tsx
@@ -3,22 +3,36 @@ import { MedicamentsList } from '@/features/medicaments/components/MedicamentsLi
 import { getAllMedicaments } from '@/features/medicaments/dal'
 import { Suspense } from 'react'
 
+function Loading() {
+	return (
+		<div className='flex gap-6 h-full py-4'>
+			<div className='w-full sm:w-4/12 overflow-y-auto'>
+				<div className='animate-pulse'>
+					<div className='h-4 bg-gray-200 rounded w-3/4 mb-4'></div>
+					<div className='h-4 bg-gray-200 rounded w-1/2 mb-4'></div>
+					<div className='h-4 bg-gray-200 rounded w-5/6'></div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
 export default async function MedicamentsPage() {
 	const medicaments = await getAllMedicaments()
 
 	return (
-		<div className='flex gap-6 h-full py-4'>
-			<div className='w-full sm:w-4/12 overflow-y-auto'>
-				<Suspense fallback={<div>Loading medicaments...</div>}>
+		<Suspense fallback={<Loading />}>
+			<div className='flex gap-6 h-full py-4'>
+				<div className='w-full sm:w-4/12 overflow-y-auto'>
 					<MedicamentsList medicaments={medicaments} />
-				</Suspense>
-			</div>
-			<div className='sm:block hidden  w-[1px] h-full bg-muted-foreground'></div>
-			<div className='sm:block hidden sm:w-8/12 overflow-y-auto'>
-				<div className='border rounded h-full p-3 flex items-center justify-center'>
-					<Heading level={2}>Нічого не вибрано</Heading>
+				</div>
+				<div className='sm:block hidden  w-[1px] h-full bg-muted-foreground'></div>
+				<div className='sm:block hidden sm:w-8/12 overflow-y-auto'>
+					<div className='border rounded h-full p-3 flex items-center justify-center'>
+						<Heading level={2}>Нічого не вибрано</Heading>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Suspense>
 	)
 }
