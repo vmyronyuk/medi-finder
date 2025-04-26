d7ac3d84c4436690f30a10e0321b6cecbf9da1e8
vmyronyuk <vitalikmur38@gmail.com>
2025-04-26 10:50:53 +0300

    Fix

--- src/app/app/medicaments/page.tsx
+++ src/app/app/medicaments/page.tsx
@@ -1,6 +1,7 @@
 import { Heading } from '@/components/typography/Heading'
 import { MedicamentsList } from '@/features/medicaments/components/MedicamentsList'
 import { getAllMedicaments } from '@/features/medicaments/dal'
+import { Suspense } from 'react'
 
 export default async function MedicamentsPage() {
 	const medicaments = await getAllMedicaments()
@@ -8,7 +9,9 @@ export default async function MedicamentsPage() {
 	return (
 		<div className='flex gap-6 h-full py-4'>
 			<div className='w-full sm:w-4/12 overflow-y-auto'>
-				<MedicamentsList medicaments={medicaments} />
+				<Suspense fallback={<div>Loading medicaments...</div>}>
+					<MedicamentsList medicaments={medicaments} />
+				</Suspense>
 			</div>
 			<div className='sm:block hidden  w-[1px] h-full bg-muted-foreground'></div>
 			<div className='sm:block hidden sm:w-8/12 overflow-y-auto'>
